731756b0030f094a0ec497cc282a7adeffac3a22
beipym <payamb.wrk@gmail.com>
2023-10-30 15:16:27 +0330

    add tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `PayamB`,
+    description: `this is where i shine !`,
+    author: `https://github.com/beipym`,
+    siteUrl: `https://payamb.dev`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -39,5 +39,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-postcss',
   ],
 }
