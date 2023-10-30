76edac31cf947fa4b092b87f821eae1b08bbf810
beipym <payamb.wrk@gmail.com>
2023-10-30 16:39:12 +0330

    initial design for introblock

--- src/components/home/introBlock.js
+++ src/components/home/introBlock.js
@@ -4,8 +4,16 @@ import { useEffect , useState } from 'react'
 const IntroBlock = () => {
   
   return (
-    <div className="" onClick={()=>{}}>
-        this is the intro block 
+    <div className="flex flex-col w-full h-full " onClick={()=>{}}>
+
+      <div className='flex flex-col mt-40 mb-40'>
+
+        <span className=' text-4xl'> Hi 👋🏻 </span>
+        <br/>
+        <span className='mt-4 text-4xl'> My Name is Payam </span>
+
+      </div>
+      
     </div>  
   )}
 
